75c43f2937ff470a234b9a54325a3dd83bd0b537
Sam Daived <sam@dsl-hkibng11-50dca5-134.dhcp.inet.fi>
2019-03-03 16:18:45 +0200

    Fin

---
+++ public/main.js
@@ -0,0 +1,25 @@
+
+console.log("worked");
+
+
+
+document.addEventListener("submit",(p)=>{
+    p.preventDefault();
+    const searchValue =document.getElementById("inp").value;
+    var Message1=document.getElementById("list1");
+    var Message2=document.getElementById("list2");
+    var Message3=document.getElementById("list3");
+
+    fetch('http://localhost:3000/weather?address='+searchValue)
+    .then((res)=>{
+        res.json().then(re=>{
+            if(re.error){
+                return Message =re.error
+            }
+            Message1.innerHTML=re.name;
+            Message2.innerHTML=re.temperature;
+            Message3.innerHTML=re.summary})})   
+    .catch()
+    
+    
+})
